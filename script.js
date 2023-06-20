803868a38c6ecd966e8ffe030c21d302e7d3325c
robss31 <robss31@hotmail.com>
2023-06-20 00:33:35 -0300

    Atualização dos index, style, img e script

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
 
-let idx = 0;
+// ANIMAiS, CASA,
+const imgs = document.getElementById("slide");
+const img = document.querySelectorAll("#slide img");
+
+let idf = 0;
 
 function carousel() {
-    idx++;
+    idf++;
 
-    if (idx > img.length - 2){
-        idx = 0;
+    if (idf > img.length - 2) {
+        idf = 0;
     }
 
-    imgs.style.transform = `translateX(${-idx * 563}px)`;
+    imgs.style.transform = `translateX(${-idf * 219}px)`;
 }
 
-setInterval(carousel, 2000);
+setInterval(carousel, 2500);
 
+
+/*
+//CARROS
+
+const cars = document.getElementById("slide1")
+const car = document.querySelectorAll("#slide1 img")
+
+let idc = 0;
+
+function carros() {
+    idc++;
+
+    if (idc > car.length - 2) {
+        idc = 0;
+    }
+
+    cars.style.transform = `translateX(${-idc * 201.5}px)`;
+}
+
+setInterval(carros, 1000);
+
+*/
+
+const filmes = document.getElementById("slide2")
+const filme = document.querySelectorAll("#slide2 img")
+
+let ids = 0;
+
+function cinema() {
+    ids++;
+
+    if (ids > filme.length - 1) {
+        ids = 0;
+    }
+
+    filmes.style.transform = `translateX(${-ids * 197}px)`;
+}
+
+setInterval(cinema, 2500);
