edca0c3e74319ffc19b9ad15534f7a1f61dafc5d
Robson Soares <90120510+robss31@users.noreply.github.com>
2023-06-25 19:00:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -40,26 +40,26 @@ setInterval(cinema, 2500);
 
 function animais(){
     window.location.replace("http://127.0.0.1:5500/animais.html");
-    //window.location.replace("https://robss31.github.io/Carousel-em-JavaScript/animais.html");
+    window.location.replace("https://robss31.github.io/Carousel-em-JavaScript/animais.html");
 }
 
 function casas(){
     window.location.replace("http://127.0.0.1:5500/casas.html");
-    //window.location.replace("https://robss31.github.io/Carousel-em-JavaScript/casas.html");
+    window.location.replace("https://robss31.github.io/Carousel-em-JavaScript/casas.html");
 }
 
 function carros(){
     window.location.replace("http://127.0.0.1:5500/carros.html");
-   // window.location.replace("https://robss31.github.io/Carousel-em-JavaScript/carros.html");
+    window.location.replace("https://robss31.github.io/Carousel-em-JavaScript/carros.html");
 }
 
 function deco_sala(){
     window.location.replace("http://127.0.0.1:5500/deco_sala.html");
-   // window.location.replace("https://robss31.github.io/Carousel-em-JavaScript/deco_sala.html");
+    window.location.replace("https://robss31.github.io/Carousel-em-JavaScript/deco_sala.html");
 
 }
 
 function voltar(){
     window.location.replace("http://127.0.0.1:5500/index.html");
-   // window.location.replace("https://robss31.github.io/Carousel-em-JavaScript");
+    window.location.replace("https://robss31.github.io/Carousel-em-JavaScript");
 }
